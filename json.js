0312011ede5c076a3117418b2a0e6f6ac66fe549
HamzaJirah <hamzajirah@gmail.com>
2018-09-19 21:03:31 +0100

    debugged uncaught syntax error

--- json.js
+++ json.js
@@ -1,10 +1,10 @@
 //create object literal
-const student = {
+let student = {
   name: "Jirah",
   age: 28,
   address: "Abuja"
 }
 
 //convert 'student' object to json
-student = JSON.stringify();
+student = JSON.stringify(student);
 console.log(student);
