9b7e63bc9521cca2df494d43aacd34ab9eadc529
HamzaJirah <hamzajirah@gmail.com>
2018-09-19 22:31:01 +0100

    debugged code

--- students-json.js
+++ students-json.js
@@ -3,8 +3,8 @@ let httpRequest = new XMLHttpRequest();
 //write httpRequest function
 httpRequest.onload = function(){
   if(this.status === 200){
-    const reponse = httpRequest.responseText
-    console.log(students.age);
+    const reponse = JSON.parse(httpRequest.responseText);
+    console.log(reponse.students);
   };
 }
 httpRequest.open("GET", "students.json", true);
