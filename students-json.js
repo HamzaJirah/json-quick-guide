a3d4b5531e6953c5caed718117acb6d0b83642ba
HamzaJirah <hamzajirah@gmail.com>
2018-09-19 22:25:55 +0100

    debugged error

--- students-json.js
+++ students-json.js
@@ -2,9 +2,9 @@
 let httpRequest = new XMLHttpRequest();
 //write httpRequest function
 httpRequest.onload = function(){
-  httpRequest.open("GET", "students.json", true);
   if(this.status === 200){
     console.log(httpRequest.responseText);
-  }
-  httpRequest.send();
+  };
 }
+httpRequest.open("GET", "students.json", true);
+httpRequest.send()
